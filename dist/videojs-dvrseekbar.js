0e7d05bc799315e1f39631a5be7734e75dfd22af
Clément Thoret <cthoret@u-pro.fr>
2018-11-05 17:03:05 +0100

    mode videojs in devdependency

--- dist/videojs-dvrseekbar.js
+++ dist/videojs-dvrseekbar.js
@@ -1,10 +1,10 @@
 /**
  * videojs-dvrseekbar
  * @version 0.2.6
- * @copyright 2016 ToolBox-tve
+ * @copyright 2018 ToolBox-tve
  * @license Apache-2.0
  */
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.videojsDvrseekbar = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.videojsDvrseekbar = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (global){
 'use strict';
 
@@ -85,7 +85,9 @@ var onTimeUpdate = function onTimeUpdate(player, e) {
   var time = player.seekable();
   var btnLiveEl = document.getElementById('liveButton');
 
-  if (!time.length) {
+  // When any tech is disposed videojs will trigger a 'timeupdate' event when calling stopTrackingCurrentTime()
+  // If the tech does not have a seekable() method, time will be undefined
+  if (!time || !time.length) {
     return;
   }
 
