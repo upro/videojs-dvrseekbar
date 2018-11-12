0d14288fba95b19b4dd7167f5f51dfc7e31b66aa
Clément Thoret <cthoret@u-pro.fr>
2018-11-12 15:36:43 +0100

    update plungin in class advanced

--- dist/videojs-dvrseekbar.js
+++ dist/videojs-dvrseekbar.js
@@ -7,165 +7,215 @@
 
   videojs = videojs && videojs.hasOwnProperty('default') ? videojs['default'] : videojs;
 
+  var classCallCheck = function (instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  };
+
+  var inherits = function (subClass, superClass) {
+    if (typeof superClass !== "function" && superClass !== null) {
+      throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+    }
+
+    subClass.prototype = Object.create(superClass && superClass.prototype, {
+      constructor: {
+        value: subClass,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+    if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+  };
+
+  var possibleConstructorReturn = function (self, call) {
+    if (!self) {
+      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    }
+
+    return call && (typeof call === "object" || typeof call === "function") ? call : self;
+  };
+
+  var Plugin = videojs.getPlugin('plugin');
+
   // Default options for the plugin.
-  var defaults = {
+  var defaults$1 = {
     startTime: 0
   };
 
-  /**
-   * Function to invoke when the player is ready.
-   *
-   * This is a great place for your plugin to initialize itself. When this
-   * function is called, the player will have its DOM and child components
-   * in place.
-   *
-   * @function onPlayerReady
-   * @param    {Player} player
-   * @param    {Object} [options={}]
-   */
-  var onPlayerReady = function onPlayerReady(player, options) {
-    player.addClass('vjs-dvrseekbar');
-    player.controlBar.addClass('vjs-dvrseekbar-control-bar');
+  var Dvrseekbar = function (_Plugin) {
+    inherits(Dvrseekbar, _Plugin);
 
-    if (player.controlBar.progressControl) {
-      player.controlBar.progressControl.addClass('vjs-dvrseekbar-progress-control');
-    }
+    function Dvrseekbar(player, options) {
+      classCallCheck(this, Dvrseekbar);
 
-    // ADD Live Button:
-    var btnLiveEl = document.createElement('div'),
-        newLink = document.createElement('button');
+      var _this = possibleConstructorReturn(this, _Plugin.call(this, player));
 
-    btnLiveEl.className = 'vjs-live-button vjs-control';
+      _this.btnLiveEl = null;
+      _this.oldSeekBar = null;
 
-    newLink.innerHTML = document.getElementsByClassName('vjs-live-display')[0].innerHTML;
-    newLink.id = 'liveButton';
+      _this.options = videojs.mergeOptions(defaults$1, options);
 
-    if (!player.paused()) {
-      newLink.className = 'vjs-live-label onair';
-    }
+      _this.player.ready(function () {
+        _this.player.addClass('vjs-dvrseekbar');
+        _this.player.controlBar.addClass('vjs-dvrseekbar-control-bar');
 
-    var clickHandler = function clickHandler(e) {
-      player.currentTime(player.seekable().end(0));
+        if (_this.player.controlBar.progressControl) {
+          _this.player.controlBar.progressControl.addClass('vjs-dvrseekbar-progress-control');
+        }
 
-      player.play();
-    };
+        // ADD Live Button:
+        var btnLiveEl = document.createElement('div');
+        _this.newLink = document.createElement('button');
 
-    if (newLink.addEventListener) {
-      // DOM method
-      newLink.addEventListener('click', clickHandler, false);
-    } else if (newLink.attachEvent) {
-      // this is for IE, because it doesn't support addEventListener
-      newLink.attachEvent('onclick', function () {
-        return clickHandler.apply(newLink, [window.event]);
+        btnLiveEl.className = 'vjs-live-button vjs-control';
+
+        _this.newLink.innerHTML = document.getElementsByClassName('vjs-live-display')[0].innerHTML;
+        _this.newLink.id = 'liveButton';
+
+        if (!player.paused()) {
+          _this.newLink.className = 'vjs-live-label onair';
+        }
+
+        var clickHandler = function clickHandler(e) {
+          player.currentTime(player.seekable().end(0));
+
+          player.play();
+        };
+
+        if (_this.newLink.addEventListener) {
+          // DOM method
+          _this.newLink.addEventListener('click', clickHandler, false);
+        } else if (_this.newLink.attachEvent) {
+          // this is for IE, because it doesn't support addEventListener
+          _this.newLink.attachEvent('onclick', function () {
+            return clickHandler.apply(newLink, [window.event]);
+          });
+        }
+
+        btnLiveEl.appendChild(_this.newLink);
+
+        var controlBar = document.getElementsByClassName('vjs-control-bar')[0],
+            insertBeforeNode = document.getElementsByClassName('vjs-progress-control')[0];
+
+        controlBar.insertBefore(btnLiveEl, insertBeforeNode);
+
+        videojs.log('dvrSeekbar Plugin ENABLED!');
       });
+
+      _this.setSeekBar();
+      _this.init();
+      return _this;
     }
 
-    btnLiveEl.appendChild(newLink);
+    Dvrseekbar.prototype.init = function init() {
+      var _this2 = this;
 
-    var controlBar = document.getElementsByClassName('vjs-control-bar')[0],
-        insertBeforeNode = document.getElementsByClassName('vjs-progress-control')[0];
+      this.player.on('timeupdate', function (e) {
+        _this2.onTimeUpdate(_this2, e);
+      });
 
-    controlBar.insertBefore(btnLiveEl, insertBeforeNode);
+      this.player.on('play', function (e) {});
 
-    videojs.log('dvrSeekbar Plugin ENABLED!', options);
-  };
+      this.player.on('pause', function (e) {
+        var btnLiveEl = document.getElementById('liveButton');
 
-  var onTimeUpdate = function onTimeUpdate(player, e) {
-    var time = player.seekable();
-    var btnLiveEl = document.getElementById('liveButton');
+        btnLiveEl.className = 'vjs-live-label';
+      });
 
-    // When any tech is disposed videojs will trigger a 'timeupdate' event when calling stopTrackingCurrentTime()
-    // If the tech does not have a seekable() method, time will be undefined
-    if (!time || !time.length) {
-      return;
-    }
-
-    /*let time1 = time && time.length ? time.end(0) - time.start(0) : 0;
-     if(time1 > 0) {
-      player.duration(time1 + 2);
-    }
-    */
-
-    if (time.end(0) - player.currentTime() < 30) {
-
-      btnLiveEl.className = 'vjs-live-label onair';
-    } else {
-      btnLiveEl.className = 'vjs-live-label';
-    }
-
-    player.duration(player.seekable().end(0));
-  };
-
-  /**
-   * A video.js plugin.
-   *
-   * In the plugin function, the value of `this` is a video.js `Player`
-   * instance. You cannot rely on the player being in a "ready" state here,
-   * depending on how the plugin is invoked. This may or may not be important
-   * to you; if not, remove the wait for "ready"!
-   *
-   * @function dvrseekbar
-   * @param    {Object} [options={}]
-   *           An object of options left to the plugin author to define.
-   */
-  var dvrseekbar = function dvrseekbar(options) {
-    var _this = this;
-    var SeekBar = videojs.getComponent('SeekBar');
-
-    SeekBar.prototype.dvrTotalTime = function (player) {
-      var time = player.seekable();
-
-      return time && time.length ? time.end(0) - time.start(0) : 0;
+      // this.player.ready(() => {
+      //   onPlayerReady(this, videojs.mergeOptions(this.defaults, this.options));
+      // });
     };
 
-    SeekBar.prototype.handleMouseMove = function (e) {
-      var bufferedTime = void 0,
-          newTime = void 0;
+    Dvrseekbar.prototype.setSeekBar = function setSeekBar() {
+      this.oldSeekBar = videojs.getComponent('SeekBar');
+      var SeekBar = videojs.getComponent('SeekBar');
 
-      bufferedTime = newTime = this.player_.seekable();
+      SeekBar.prototype.dvrTotalTime = function (player) {
+        var time = player.seekable();
 
-      if (bufferedTime && bufferedTime.length) {
-        for (newTime = bufferedTime.start(0) + this.calculateDistance(e) * this.dvrTotalTime(this.player_); newTime >= bufferedTime.end(0);) {
-          newTime -= .1;
-        }this.player_.currentTime(newTime);
-      }
+        return time && time.length ? time.end(0) - time.start(0) : 0;
+      };
+
+      SeekBar.prototype.handleMouseMove = function (e) {
+        var bufferedTime = void 0,
+            newTime = void 0;
+
+        bufferedTime = newTime = this.player_.seekable();
+
+        if (bufferedTime && bufferedTime.length) {
+          for (newTime = bufferedTime.start(0) + this.calculateDistance(e) * this.dvrTotalTime(this.player_); newTime >= bufferedTime.end(0);) {
+            newTime -= .1;
+          }this.player_.currentTime(newTime);
+        }
+      };
+
+      SeekBar.prototype.updateAriaAttributes = function () {
+        var a = void 0,
+            c = void 0,
+            d = this.player_.seekable();
+
+        d && d.length && (a = this.player_.scrubbing ? this.player_.getCache().currentTime : this.player_.currentTime(), c = d.end(0) - a, c = 0 > c ? 0 : c, this.el_.setAttribute('aria-valuenow', Math.round(100 * this.getPercent(), 2)), this.el_.setAttribute('aria-valuetext', (0 === a ? "" : "-") + videojs.formatTime(c, d.end(0))));
+      };
     };
 
-    SeekBar.prototype.updateAriaAttributes = function () {
-      var a = void 0,
-          c = void 0,
-          d = this.player_.seekable();
-
-      d && d.length && (a = this.player_.scrubbing ? this.player_.getCache().currentTime : this.player_.currentTime(), c = d.end(0) - a, c = 0 > c ? 0 : c, this.el_.setAttribute('aria-valuenow', Math.round(100 * this.getPercent(), 2)), this.el_.setAttribute('aria-valuetext', (0 === a ? "" : "-") + videojs.formatTime(c, d.end(0))));
-    };
-
-    if (!options) {
-      options = defaults;
-    }
-
-    this.on('timeupdate', function (e) {
-      onTimeUpdate(_this, e);
-    });
-
-    this.on('play', function (e) {});
-
-    this.on('pause', function (e) {
+    Dvrseekbar.prototype.onTimeUpdate = function onTimeUpdate() {
+      var time = this.player.seekable();
       var btnLiveEl = document.getElementById('liveButton');
 
-      btnLiveEl.className = 'vjs-live-label';
-    });
+      // When any tech is disposed videojs will trigger a 'timeupdate' event when calling stopTrackingCurrentTime()
+      // If the tech does not have a seekable() method, time will be undefined
+      if (!time || !time.length) {
+        return;
+      }
 
-    this.ready(function () {
-      onPlayerReady(_this, videojs.mergeOptions(defaults, options));
-    });
-  };
+      /*let time1 = time && time.length ? time.end(0) - time.start(0) : 0;
+         if(time1 > 0) {
+        player.duration(time1 + 2);
+      }
+      */
 
+      if (time.end(0) - this.player.currentTime() < 30) {
+
+        btnLiveEl.className = 'vjs-live-label onair';
+      } else {
+        btnLiveEl.className = 'vjs-live-label';
+      }
+
+      this.player.duration(this.player.seekable().end(0));
+    };
+
+    Dvrseekbar.prototype.dispose = function dispose() {
+      this.player.removeClass('vjs-dvrseekbar');
+      this.player.controlBar.removeClass('vjs-dvrseekbar-control-bar');
+      if (this.player.controlBar.progressControl) {
+        this.player.controlBar.progressControl.removeClass('vjs-dvrseekbar-progress-control');
+      }
+
+      Array.from(document.getElementsByClassName('vjs-live-button')).forEach(function (element) {
+        return element.remove();
+      });
+
+      //vjs-dvrseekbar-progress-control  vjs-progress-control vjs-control vjs-dvrseekbar-progress-control
+
+      //vjs-live-button vjs-control
+      this.player.off('timeupdate');
+      _Plugin.prototype.dispose.call(this);
+      videojs.log('the advanced plugin is being disposed');
+    };
+
+    return Dvrseekbar;
+  }(Plugin);
   // Register the plugin with video.js.
-  videojs.plugin('dvrseekbar', dvrseekbar);
+
+
+  videojs.registerPlugin('dvrseekbar', Dvrseekbar);
 
   // Include the version number.
-  dvrseekbar.VERSION = '__VERSION__';
+  Dvrseekbar.VERSION = '__VERSION__';
 
-  return dvrseekbar;
+  return Dvrseekbar;
 
 })));
